3a4d553047dddd55c23764637aae765e292089dd
Nawin Tamang <sachinsherpa11@gmail.com>
2025-03-18 13:29:29 +0545

    bronze layer create table scripts

--- scripts/bronze_layer/bronze_DDL.sql
+++ scripts/bronze_layer/bronze_DDL.sql
@@ -31,8 +31,6 @@ GO
 
 IF OBJECT_ID('bronze.crm_customer_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_customer_info;
-GO
-
 CREATE TABLE bronze.crm_customer_info (
 	cst_id	INT,
 	cst_key	NVARCHAR(50),
@@ -42,13 +40,10 @@ CREATE TABLE bronze.crm_customer_info (
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
-GO
 
 
 IF OBJECT_ID('bronze.crm_product_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_product_info;
-GO
-
 CREATE TABLE bronze.crm_product_info (
 	prd_id	INT,
 	prd_key	NVARCHAR(50),
@@ -58,13 +53,10 @@ CREATE TABLE bronze.crm_product_info (
 	prd_start_dt DATETIME,
 	prd_end_dt DATETIME
 );
-GO
 
 
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
-GO
-
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
@@ -76,36 +68,27 @@ CREATE TABLE bronze.crm_sales_details (
     sls_quantity INT,
     sls_price INT
 );
-GO
 
 
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
-GO
-
 CREATE TABLE bronze.erp_loc_a101 (
     cid    NVARCHAR(50),
     cntry  NVARCHAR(50)
 );
-GO
 
 
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
-GO
-
 CREATE TABLE bronze.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
     gen    NVARCHAR(50)
 );
-GO
 
 
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
-GO
-  
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
     cat          NVARCHAR(50),
